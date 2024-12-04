d3b73c3f84b227e915eb40f616dca94f664ee46f
THERESIAOSAMOSIR <samosirtheresia23@gmail.com>
2024-12-04 17:10:30 +0700

    Done

--- X02.java
+++ X02.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class T06 {
+public class X02 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
